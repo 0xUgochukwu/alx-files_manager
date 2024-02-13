9fbaf39983d8c2a5d518ec9190a336c007e6d825
0xUgochukwu <0xUgochukwu@gmail.com>
2024-02-13 14:26:52 +0100

    refactor: getShow

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -66,19 +66,18 @@ export default class FilesController {
   static async getShow(request, response) {
     const { id } = request.params;
     const _id = new ObjectId(id);
-    const token = request.headers['x-token'];
-    const userId = await redisClient.get(`auth_${token}`);
     const file = await dbClient.findFile(_id);
-    if (file && userId) {
-      const ID = new ObjectId(userId);
-      const user = await dbClient.findUser(ID);
-      if (!user) {
-        response.status(401).json({ error: 'Unauthorized' });
-      } else if (userId === file.userId) {
-        response.status(200).json(file);
-      } else {
-        response.status(404).json({ error: 'Not found' });
-      }
+    if (request.user._id === file.userId) {
+      response.status(200).json(file);
+    } else {
+      response.status(404).json({ error: 'Not found' });
     }
+  }
+
+  static async putPublish(request, response) {
+    const { id } = request.params;
+
+
+
   }
 }
